c58fb82ab5eb7c73898c080e06983fdc27d1e409
Chris Trevino <chris.trevino@atsid.com>
2015-04-23 13:09:18 -0700

    Update the Run script with correct env. vars

--- run.sh
+++ run.sh
@@ -1,34 +1,34 @@
-if [ ! -n "$WERCKER_EB_DEPLOY_ACCESS_KEY" ]; then
+if [ ! -n "$WERCKER_EB_DEPLOY_S3_ACCESS_KEY" ]; then
   error 'Please specify access-key'
   exit 1
 fi
 
-if [ ! -n "$WERCKER_EB_DEPLOY_SECRET_KEY" ]; then
+if [ ! -n "$WERCKER_EB_DEPLOY_S3_SECRET_KEY" ]; then
   error 'Please specify secret-key'
   exit 1
 fi
 
-if [ ! -n "$WERCKER_EB_DEPLOY_APP_NAME" ]; then
+if [ ! -n "$WERCKER_EB_DEPLOY_S3_APP_NAME" ]; then
   error 'Please specify app-name'
   exit 1
 fi
 
-if [ ! -n "$WERCKER_EB_DEPLOY_ENV_NAME" ]; then
+if [ ! -n "$WERCKER_EB_DEPLOY_S3_ENV_NAME" ]; then
   error 'Please specify env-name'
   exit 1
 fi
 
-if [ ! -n "$WERCKER_EB_DEPLOY_VERSION_LABEL" ]; then
+if [ ! -n "$WERCKER_EB_DEPLOY_S3_VERSION_LABEL" ]; then
   error 'Please specify version-label'
   exit 1
 fi
 
-if [ ! -n "$WERCKER_EB_DEPLOY_S3_BUCKET" ]; then
+if [ ! -n "$WERCKER_EB_DEPLOY_S3_S3_BUCKET" ]; then
   error 'Please specify s3 bucket'
   exit 1
 fi
 
-if [ ! -n "$WERCKER_EB_DEPLOY_S3_KEY" ]; then
+if [ ! -n "$WERCKER_EB_DEPLOY_S3_S3_KEY" ]; then
   error 'Please specify s3 key'
   exit 1
 fi
